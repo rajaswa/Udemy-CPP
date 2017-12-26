ec6f3bb1ca2965c7ac7129ad0a24f16fd19d772b
rajaswa <rajp4480@gmail.com>
2017-12-26 18:02:18 +0530

    this is a test commit.

--- 7-Functions/7-Functions/EXERCISES for Functions/fibonacci.cpp
+++ 7-Functions/7-Functions/EXERCISES for Functions/fibonacci.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 
 int fibonacci(int);
+int fibonacciWithRecursion(int);
 
 int main() {
     int term;
     cout << "Which term of the fibonacci sequence do you want ? " << endl;
     cin >> term;
-    cout << fibonacci(term);
+    cout << fibonacci(term)<<endl;
+    cout << fibonacciWithRecursion(term);
 
 }
 
@@ -23,11 +25,19 @@ int fibonacci(int term) {
         c = a + b;
         a = b;
         b = c;
-
-
     }
-
     return c;
 }
 
+int fibonacciWithRecursion(int term){
+
+    if (term ==0){
+        return 0;
+    }
+    if (term ==1 || term ==2){
+        return 1;
+    }
+    return fibonacciWithRecursion(term -1 ) + fibonacciWithRecursion( term -2);
+
+}
 
